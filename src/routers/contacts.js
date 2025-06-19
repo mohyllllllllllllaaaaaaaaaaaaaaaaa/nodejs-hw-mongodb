636199bdf6896e6ab8fabaa9f42c951c3bba179f
natalia mohyla <mogilesha81@gmail.com>
2025-06-19 21:45:39 +0200

    add validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import { addContact, deleteContactController, getAllContacts, getContactById, updateContactController } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { contactsSchema, updateContactSchema } from '../validation/contacts.js';
 
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getAllContacts));
-router.get('/:contactId', ctrlWrapper(getContactById));
-router.post('/', ctrlWrapper(addContact));
-router.patch('/:contactId', ctrlWrapper(updateContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post('/', validateBody(contactsSchema),  ctrlWrapper(addContact));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+export const contactsSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email().optional(),
+    isFavourite: Joi.boolean().optional(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+export const updateContactSchema = Joi.object({
+        name: Joi.string().min(3).max(20).optional(),
+    phoneNumber: Joi.string().min(3).max(20).optional(),
+    email: Joi.string().email().optional(),
+    isFavourite: Joi.boolean().optional(),
+    contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+});
